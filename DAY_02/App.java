3bd622c3a5ee0047d934661007bb64e49eaf7787
oshadi <bawanioshadi0@gmail.com>
2025-04-27 19:05:11 +0530

    created App.java file

---
+++ DAY_02/App.java
@@ -0,0 +1,23 @@
+import p1.*;
+import p2.*;
+import p1.p3.*;
+
+class App{
+	
+	public static void main(String[] args){
+		
+		MyClass mc=new MyClass();
+		mc.display();
+		
+		Class2 c1=new Class2();
+		c1.display();
+		
+		Class3 c3=new Class3();
+		c3.display();
+		
+		Class4 c4=new Class4();
+		c4.display();
+		
+	}
+	
+}
