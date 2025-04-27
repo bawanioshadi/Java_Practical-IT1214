3a0300555510e7b1c6439e86309bd5ef4096075c
oshadi <bawanioshadi0@gmail.com>
2025-04-27 19:06:26 +0530

    created it and amc file

---
+++ DAY_02/amc/SecondYear.java
@@ -0,0 +1,7 @@
+package amc;
+
+public class SecondYear{
+	public void display(){
+		System.out.println("Hi from second Year in package amc");
+	}
+}
---
+++ DAY_02/amc/first.java
@@ -0,0 +1,7 @@
+package amc;
+
+public class first{
+	public void display(){
+		System.out.println("Hi from First year amc i in pakage p3");
+	}
+}
